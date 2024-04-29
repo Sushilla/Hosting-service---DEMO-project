6da967e29e11ef15d5f7a41bddd64ee966b9d566
Robertas Kropa <robertas@privacypartners.lt>
2024-04-29 11:59:31 +0300

    [ADD] Added new type - domain + email

--- src/app/service-dashboard/Models/Interface/UserServerList.ts
+++ src/app/service-dashboard/Models/Interface/UserServerList.ts
@@ -1,9 +1,10 @@
 export interface UserServerList {
   current_server_plan: number;
-  server_name: string;
-  storage: number;
-  used_storage: number;
+  service_name: string;
   own_domain: boolean;
   domain: string;
+  own_server: boolean;
+  storage: number;
+  used_storage: number;
   email_protection: boolean;
 }
--- src/app/service-dashboard/service-dashboard.component.ts
+++ src/app/service-dashboard/service-dashboard.component.ts
@@ -5,25 +5,33 @@ import { ServiceListComponent } from './components/service-list/service-list.com
 import { ServerProperties } from './Models/Interface/ServerProperties';
 import ServerPlanJSON from '../../assets/data/ServerPlans.json';
 import UserPropertiesJSON from '../../assets/data/UserProperties.json';
+import PricingJSON from '../../assets/data/Pricing.json';
 import { UserServerList } from './Models/Interface/UserServerList';
 import { CommonModule } from '@angular/common';
+import { Pricing } from './Models/Interface/Pricing';
 
 @Component({
   selector: 'app-service-dashboard',
   standalone: true,
-  imports: [HeaderComponent, ServerResourcesComponent, ServiceListComponent, CommonModule],
+  imports: [
+    HeaderComponent,
+    ServerResourcesComponent,
+    ServiceListComponent,
+    CommonModule,
+  ],
   templateUrl: './service-dashboard.component.html',
   styleUrl: './service-dashboard.component.scss',
 })
 export class ServiceDashboardComponent {
   UserServersList: UserServerList[] = UserPropertiesJSON;
   ServerPlans: ServerProperties[] = ServerPlanJSON;
+  PricingForService: Pricing = PricingJSON;
 
   SelectedService!: UserServerList | undefined;
 
   constructor() {
     console.log(this.ServerPlans);
     console.log(this.UserServersList[0]);
+    console.log(this.PricingForService);
   }
-
 }
